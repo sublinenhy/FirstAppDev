b3c7f9bd31a96c4944d9dd3d1f14d98f2acb9b6b
camden <3293676794@qq.com>
2019-06-12 13:42:15 +0800

    firebase引入，实时数据库测试成功，login界面布局完成

---
+++ app/src/main/java/com/example/camden/greenhandproject/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.camden.greenhandproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gyf.immersionbar.ImmersionBar;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ImmersionBar.with(this)
+                .statusBarDarkFont(true, 0.2f) //原理：如果当前设备支持状态栏字体变色，会设置状态栏字体为黑色，如果当前设备不支持状态栏字体变色，会使当前状态栏加上透明度，否则不执行透明度
+                .fitsSystemWindows(true)  //使用该属性,必须指定状态栏颜色
+                .statusBarColor(R.color.colorPrimary)
+                .init();
+
+
+
+    }
+}
--- app/src/main/java/com/example/camden/greenhandproject/MainActivity.java
+++ app/src/main/java/com/example/camden/greenhandproject/MainActivity.java
@@ -18,6 +18,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.gyf.immersionbar.ImmersionBar;
 
 import java.util.LinkedList;
@@ -28,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager mLayoutManager;
     private NoteDatabaseHelper dbHelper;
     private LinkedList<NoteBean> notes;
-    private NoteBean noteBean;
+    private static final String TAG = "MainActivity";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mLayoutManager);
         MyAdapter mAdapter = new MyAdapter();
         mRecyclerView.setAdapter(mAdapter);
+        basicReadWrite();
 
 
 
@@ -62,6 +71,35 @@ public class MainActivity extends AppCompatActivity {
         Intent intent=new Intent(MainActivity.this, EditActivity.class);
         startActivity(intent);
 
+    }
+
+    public void basicReadWrite() {
+        // [START write_message]
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message/test1");
+
+        myRef.setValue("Hello, World!");
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        // [END read_message]
     }
 
 
